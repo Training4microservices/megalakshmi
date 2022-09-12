1182422c68668898944c078226de56c60958432d
Megalakshmi <megark39001@gmail.com>
2022-09-12 12:14:03 +0000

    Added code

---
+++ Spring-Boot-JMS-ActiveMQw/Spring-Boot-JMS-ActiveMQw/src/main/java/com/example/SpringBootJMSActiveMQw/SpringBootJmsActiveMQwApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootJMSActiveMQw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJmsActiveMQwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJmsActiveMQwApplication.class, args);
+	}
+
+}
---
+++ Spring-Boot-JMS-ActiveMQw/Spring-Boot-JMS-ActiveMQw/src/test/java/com/example/SpringBootJMSActiveMQw/SpringBootJmsActiveMQwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootJMSActiveMQw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJmsActiveMQwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
